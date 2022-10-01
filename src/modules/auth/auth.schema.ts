f8d78be8de2e6edbf6d758ed4fb3969031632202
Jan Henning <triton539@outlook.com>
2022-10-01 17:33:13 +0200

    Updates /auth schemas and route

--- src/modules/auth/auth.schema.ts
+++ src/modules/auth/auth.schema.ts
@@ -1,21 +1,15 @@
 import { z } from "zod"
-import { buildJsonSchemas } from "fastify-zod"
 
-const loginSchema = z.object({
+export const loginSchema = z.object({
   username: z.string({
     required_error: "Username is required",
     invalid_type_error: "Username must be a string"
   })
 })
 
-const authResponseSchema = z.object({
+export const authResponseSchema = z.object({
   accessToken: z.string()
 })
 
 export type LoginInput = z.infer<typeof loginSchema>
 export type AuthSchema = z.infer<typeof authResponseSchema>
-
-export const { schemas: authSchemas, $ref } = buildJsonSchemas(
-  { loginSchema, authResponseSchema },
-  { $id: "Auth" }
-)
