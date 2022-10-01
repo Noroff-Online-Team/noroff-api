dff2ae706f21c31eaacac4f5d70d172892427baf
Jan Henning <triton539@outlook.com>
2022-10-01 16:33:44 +0200

    server.ts consistent formatting

--- src/server.ts
+++ src/server.ts
@@ -3,34 +3,34 @@ import Fastify, { FastifyRequest, FastifyReply } from "fastify"
 import cors from "@fastify/cors"
 import swagger from "@fastify/swagger"
 import fStatic from "@fastify/static"
-import fJwt from '@fastify/jwt'
-import fAuth from '@fastify/auth'
-import fRateLimit from '@fastify/rate-limit'
+import fJwt from "@fastify/jwt"
+import fAuth from "@fastify/auth"
+import fRateLimit from "@fastify/rate-limit"
 
-import swaggerOptions from './config/swagger'
+import swaggerOptions from "./config/swagger"
 
 // Route imports
 import statusRoutes from "./modules/status/status.route"
-import authRoutes from './modules/auth/auth.route'
+import authRoutes from "./modules/auth/auth.route"
 import bookRoutes from "./modules/books/books.route"
 import catFactRoutes from "./modules/catFacts/catFacts.route"
 import jokeRoutes from "./modules/jokes/jokes.route"
 import nbaTeamRoutes from "./modules/nbaTeams/nbaTeams.route"
 import oldGameRoutes from "./modules/oldGames/oldGames.route"
-import quotesRoutes from './modules/quotes/quotes.route'
+import quotesRoutes from "./modules/quotes/quotes.route"
 import postsRoutes from "./modules/social/posts/posts.route"
 import profilesRoutes from "./modules/social/profiles/profiles.route"
 import socialAuthRoutes from "./modules/social/auth/auth.route"
 
 // Schema imports
 import { statusSchemas } from "./modules/status/status.schema"
-import { authSchemas } from './modules/auth/auth.schema'
+import { authSchemas } from "./modules/auth/auth.schema"
 import { bookSchemas } from "./modules/books/books.schema"
 import { catFactSchemas } from "./modules/catFacts/catFacts.schema"
 import { jokeSchemas } from "./modules/jokes/jokes.schema"
 import { nbaTeamSchemas } from "./modules/nbaTeams/nbaTeams.schema"
 import { oldGameSchemas } from "./modules/oldGames/oldGames.schema"
-import { quoteSchemas } from './modules/quotes/quotes.schema'
+import { quoteSchemas } from "./modules/quotes/quotes.schema"
 import { postSchemas } from "./modules/social/posts/posts.schema"
 import { profileSchemas } from "./modules/social/profiles/profiles.schema"
 import { socialAuthSchemas } from "./modules/social/auth/auth.schema"
@@ -56,7 +56,7 @@ function buildServer() {
   // Register rate-limit
   server.register(fRateLimit, {
     max: 30,
-    timeWindow: '1 minute',
+    timeWindow: "1 minute"
   })
 
   // Register CORS
@@ -77,7 +77,7 @@ function buildServer() {
   // Register Auth
   server.register(fAuth)
 
-  server.addContentTypeParser('application/json', {parseAs: 'string'}, (_request, body, done) => {
+  server.addContentTypeParser("application/json", { parseAs: "string" }, (_request, body, done) => {
     if (!body) {
       done(null)
     }
@@ -100,9 +100,9 @@ function buildServer() {
 
   // Add JWT to the request object so we can access it in our controllers.
   server.addHook("preHandler", (req, reply, next) => {
-    req.jwt = server.jwt;
-    return next();
-  });
+    req.jwt = server.jwt
+    return next()
+  })
 
   // Register schemas so they can be referenced in our routes
   for (const schema of allSchemas) {
