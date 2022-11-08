70f0b2b48992aebe6bef81e4748acc3a0888c7bc
Jan Henning <triton539@outlook.com>
2022-11-08 17:46:52 +0100

    Transfer all non-winning bids back after listing ends

--- src/modules/auction/listings/listing.utils.ts
+++ src/modules/auction/listings/listing.utils.ts
@@ -11,15 +11,30 @@ import { ListingWithBids } from "./listings.controller"
  */
 export async function scheduleCreditsTransfer(listingId: string, endsAt: Date): Promise<void> {
   schedule.scheduleJob(endsAt, async () => {
+    // Get listing
     const listing = (await getListing(listingId, { bids: true })) as ListingWithBids
 
     if (listing.bids.length > 0) {
+      // Get highest bid of listing
       const highestBid = Math.max(...listing.bids.map(bid => bid.amount), 0)
 
+      // Transfer credits from highest bid to seller
       await prisma.auctionProfile.update({
         where: { name: listing.sellerName },
         data: { credits: { increment: highestBid } }
       })
+
+      // Transfer all non-winning bids back to their bidders
+      await Promise.all(
+        listing.bids
+          .filter(bid => bid.amount !== highestBid)
+          .map(bid =>
+            prisma.auctionProfile.update({
+              where: { name: bid.bidderName },
+              data: { credits: { increment: bid.amount } }
+            })
+          )
+      )
     }
   })
 }
