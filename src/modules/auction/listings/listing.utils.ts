5c792d7d7c44e80e2c9112264aeb33af7083e2d5
Fermain <Fermain@users.noreply.github.com>
2022-11-09 17:03:43 +0200

    Adds cap solution

--- src/modules/auction/listings/listing.utils.ts
+++ src/modules/auction/listings/listing.utils.ts
@@ -4,6 +4,25 @@ import { getListing } from "./listings.service"
 import { ListingWithBids } from "./listings.controller"
 
 /**
+ * Either award a user credits or do nothing depending on their current credit amount
+ * @param name name of the auction profile user to locate
+ * @param increment amount of credits to award
+ * @param max maximum amount of credits that is allowed
+ */
+export async function awardCreditsOrCap(name: string, increment: number, max = 1_000_000) {
+  const user = await prisma.auctionProfile.findUnique({
+    where: { name }
+  })
+
+  if (user && user.credits + increment <= max) {
+    await prisma.auctionProfile.update({
+      where: { name },
+      data: { credits: { increment } }
+    })
+  }
+}
+
+/**
  * Schedule a cron-job to transfer winning bid credits to the seller of the listing.
  * This should be called when a listing is created.
  * @param listingId id of the listing to schedule
@@ -19,10 +38,7 @@ export async function scheduleCreditsTransfer(listingId: string, endsAt: Date): 
       const [winner, ...losers] = listing.bids.sort((a, b) => b.amount - a.amount)
 
       // Transfer credits from highest bid to seller
-      await prisma.auctionProfile.update({
-        where: { name: listing.sellerName },
-        data: { credits: { increment: winner.amount } }
-      })
+      await awardCreditsOrCap(listing.sellerName, winner.amount)
 
       // Transfer all non-winning bids back to their bidders
       await Promise.all(
