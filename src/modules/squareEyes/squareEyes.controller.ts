54b479105cac7a12d2b3277afd28c461f347f415
Jan Henning <triton539@outlook.com>
2023-03-16 20:28:35 +0100

    Add route modules

---
+++ src/modules/squareEyes/squareEyes.controller.ts
@@ -0,0 +1,24 @@
+import { FastifyRequest } from "fastify"
+import { NotFound } from "http-errors"
+
+import { getSquareEyesProducts, getSquareEyesProduct } from "./squareEyes.service"
+
+export async function getSquareEyesProductsHandler() {
+  const products = await getSquareEyesProducts()
+  return products
+}
+
+export async function getSquareEyesProductHandler(
+  request: FastifyRequest<{
+    Params: { id: string }
+  }>
+) {
+  const { id } = request.params
+  const product = await getSquareEyesProduct(id)
+
+  if (!product) {
+    throw new NotFound("No product with such ID")
+  }
+
+  return product
+}
---
+++ src/modules/squareEyes/squareEyes.route.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from "fastify"
+
+import { squareEyesResponseSchema, squareEyesParamsSchema } from "./squareEyes.schema"
+import { getSquareEyesProductsHandler, getSquareEyesProductHandler } from "./squareEyes.controller"
+
+async function squareEyesRoutes(server: FastifyInstance) {
+  server.get(
+    "/",
+    {
+      schema: {
+        tags: ["e-com"],
+        response: {
+          200: squareEyesResponseSchema.array()
+        }
+      }
+    },
+    getSquareEyesProductsHandler
+  )
+
+  server.get(
+    "/:id",
+    {
+      schema: {
+        tags: ["e-com"],
+        params: squareEyesParamsSchema,
+        response: {
+          200: squareEyesResponseSchema
+        }
+      }
+    },
+    getSquareEyesProductHandler
+  )
+}
+
+export default squareEyesRoutes
