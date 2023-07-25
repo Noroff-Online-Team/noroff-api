5dfa1839780abe9466b5f80a2603d04f44c4ef0f
Jan Henning <triton539@outlook.com>
2023-07-25 16:57:23 +0200

    Update createResponseSchema - make pageCount, totalCount optionals for when includePageCount is not set

--- src/utils/createResponseSchema.ts
+++ src/utils/createResponseSchema.ts
@@ -6,8 +6,8 @@ const metaSchema = z.object({
   currentPage: z.number().int().positive(),
   previousPage: z.number().int().positive().optional().nullable(),
   nextPage: z.number().int().positive().optional().nullable(),
-  pageCount: z.number().int().nonnegative(),
-  totalCount: z.number().int().nonnegative()
+  pageCount: z.number().int().nonnegative().optional(),
+  totalCount: z.number().int().nonnegative().optional()
 })
 
 export function createResponseSchema<T>(dataSchema: z.ZodType<T>) {
