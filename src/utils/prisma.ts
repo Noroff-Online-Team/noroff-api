a5b6de00cf50be463f9d880af999d28bfe305bd3
Jan Henning <triton539@outlook.com>
2023-07-25 13:33:47 +0200

    Install prisma-extension-pagination and setup Prisma client with it

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client"
+import { paginate } from "prisma-extension-pagination"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends({
+  model: {
+    $allModels: {
+      paginate
+    }
+  }
+})
 
 export default prisma
