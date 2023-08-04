f15c73cace7ca7aa2ce11c73fddfa0b83305f7c0
Jan Henning <triton539@outlook.com>
2023-08-05 00:02:07 +0200

    Update createResponseSchema

--- src/utils/createResponseSchema.ts
+++ src/utils/createResponseSchema.ts
@@ -1,14 +1,20 @@
 import { z } from "zod"
 
-const metaSchema = z.object({
-  isFirstPage: z.boolean(),
-  isLastPage: z.boolean(),
-  currentPage: z.number().int().positive(),
-  previousPage: z.number().int().positive().optional().nullable(),
-  nextPage: z.number().int().positive().optional().nullable(),
-  pageCount: z.number().int().nonnegative().optional(),
-  totalCount: z.number().int().nonnegative().optional()
-})
+/*
+  Defaults the meta property to empty object.
+  Pagination is not available nor make sense on PUT, POST, DELETE requests, so the returned meta object will be empty.
+*/
+const metaSchema = z
+  .object({
+    isFirstPage: z.boolean().optional(),
+    isLastPage: z.boolean().optional(),
+    currentPage: z.number().int().positive().optional(),
+    previousPage: z.number().int().positive().optional().nullable(),
+    nextPage: z.number().int().positive().optional().nullable(),
+    pageCount: z.number().int().nonnegative().optional(),
+    totalCount: z.number().int().nonnegative().optional()
+  })
+  .default({})
 
 export function createResponseSchema<T>(dataSchema: z.ZodType<T>) {
   return z.object({
