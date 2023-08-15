feb57f5da7e8f3c927e7b99a1bf3add3fd575e21
Jan Henning <triton539@outlook.com>
2023-08-15 18:34:19 +0200

    Add default user banner and avatar

--- src/modules/v2/auth/auth.service.ts
+++ src/modules/v2/auth/auth.service.ts
@@ -3,15 +3,21 @@ import { CreateProfileInput } from "./auth.schema"
 
 const DEFAULT_CREDITS = 1000
 const DEFAULT_VENUE_MANAGER = false
+const DEFAULT_AVATAR =
+  "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400"
+const DEFAULT_BANNER =
+  "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=500&w=1500"
 
 export async function createProfile(input: CreateProfileInput) {
-  const { password, ...rest } = input
+  const { password, banner, avatar, ...rest } = input
 
   const { hash, salt } = hashPassword(password)
 
   const profile = await db.userProfile.create({
     data: {
       ...rest,
+      avatar: avatar || DEFAULT_AVATAR,
+      banner: banner || DEFAULT_BANNER,
       venueManager: DEFAULT_VENUE_MANAGER,
       credits: DEFAULT_CREDITS,
       salt,
@@ -19,14 +25,12 @@ export async function createProfile(input: CreateProfileInput) {
     }
   })
 
-  return profile
+  return { data: profile }
 }
 
 export async function findProfileByEmail(email: string) {
   return await db.userProfile.findUnique({
-    where: {
-      email
-    }
+    where: { email }
   })
 }
 
