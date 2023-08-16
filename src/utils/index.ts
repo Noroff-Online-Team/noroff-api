c8fd4370a5469efe3174efa661f7dd005c791e7a
Jan Henning <triton539@outlook.com>
2023-08-16 22:21:08 +0200

    create and export objectIsEmpty util, export other utils from index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
 export { default as getRandomNumber } from "./getRandomNumber"
 export { prisma, db } from "./prisma"
+export { mediaGuard } from "./mediaGuard"
+export { createResponseSchema } from "./createResponseSchema"
+export { objectIsEmpty } from "./objectIsEmpty"
 export * from "./hash"
