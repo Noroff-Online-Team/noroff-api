e1696221fa399683ac2937f4cff82fb76186c141
Jan Henning <triton539@outlook.com>
2023-08-16 22:51:23 +0200

    Duh, objectIsEmpty isn't actually needed. Removing it.

--- src/utils/objectIsEmpty.ts
+++
@@ -1,8 +0,0 @@
-/**
- *  Check if object is empty
- * @param obj  Object to check
- * @returns  Boolean
- */
-export function objectIsEmpty(obj: Record<string, unknown>): boolean {
-  return obj && Object.keys(obj).length === 0 && obj.constructor === Object
-}
