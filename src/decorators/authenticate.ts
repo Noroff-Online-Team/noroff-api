ebe4cc5f8c341ef1fa331a2d047971f227f71b2d
Jan Henning <triton539@outlook.com>
2023-08-22 14:50:47 +0200

    Throw authenticate error instead of return

--- src/decorators/authenticate.ts
+++ src/decorators/authenticate.ts
@@ -1,12 +1,12 @@
-import { FastifyReply, FastifyRequest } from "fastify"
+import { FastifyRequest } from "fastify"
 import fp from "fastify-plugin"
 
 export default fp(async fastify => {
-  fastify.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
+  fastify.decorate("authenticate", async (request: FastifyRequest) => {
     try {
       await request.jwtVerify()
-    } catch (err) {
-      return reply.send(err)
+    } catch (error) {
+      throw error
     }
   })
 })
