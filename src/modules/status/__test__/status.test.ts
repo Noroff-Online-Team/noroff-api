c6df5b15bf6f5c58702fd3aff0742f8092636fa4
Jan Henning <triton539@outlook.com>
2023-08-24 17:41:18 +0200

    Add simple /status test

---
+++ src/modules/status/__test__/status.test.ts
@@ -0,0 +1,11 @@
+import { server } from "@/tests/server"
+
+describe("Status", () => {
+  it("should return 200 OK", async () => {
+    const response = await server.inject({ url: "/status" })
+    expect(response.statusCode).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({ status: "OK" })
+  })
+})
