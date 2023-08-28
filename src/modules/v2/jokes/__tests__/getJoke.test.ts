e1f9d4a19dd3edb1faafa25624e28dec667b3f32
Jan Henning <triton539@outlook.com>
2023-08-28 13:05:30 +0200

    Add tests for jokes

---
+++ src/modules/v2/jokes/__tests__/getJoke.test.ts
@@ -0,0 +1,82 @@
+import { server } from "@/tests/server"
+import { db } from "@/utils"
+
+beforeEach(async () => {
+  await db.$executeRaw`ALTER SEQUENCE "Joke_id_seq" RESTART WITH 1;`
+  await db.joke.createMany({
+    data: [
+      {
+        type: "general",
+        setup: "What did the fish say when it hit the wall?",
+        punchline: "Dam."
+      },
+      {
+        type: "general",
+        setup: "How do you make a tissue dance?",
+        punchline: "You put a little boogie on it."
+      }
+    ]
+  })
+})
+
+afterEach(async () => {
+  const jokes = db.joke.deleteMany()
+
+  await db.$transaction([jokes])
+  await db.$disconnect()
+})
+
+describe("[GET] /v2/jokes/:id", () => {
+  it("should return single joke based on id", async () => {
+    const response = await server.inject({
+      url: "/api/v2/jokes/1",
+      method: "GET"
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(200)
+    expect(res.data).toBeDefined()
+    expect(res.data.id).toBeDefined()
+    expect(res.data.setup).toBe("What did the fish say when it hit the wall?")
+    expect(res.meta).toBeDefined()
+    expect(res.meta).toStrictEqual({
+      isFirstPage: true,
+      isLastPage: true,
+      currentPage: 1,
+      previousPage: null,
+      nextPage: null,
+      pageCount: 1,
+      totalCount: 1
+    })
+  })
+
+  it("should return 404 if joke not found", async () => {
+    const response = await server.inject({
+      url: "/api/v2/jokes/3",
+      method: "GET"
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(404)
+    expect(res.data).not.toBeDefined()
+    expect(res.meta).not.toBeDefined()
+    expect(res.errors).toBeDefined()
+    expect(res.errors).toHaveLength(1)
+    expect(res.errors[0].message).toBe("No joke with such ID")
+  })
+
+  it("should throw zod error if id is not a number", async () => {
+    const response = await server.inject({
+      url: "/api/v2/jokes/invalid_id",
+      method: "GET"
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(400)
+    expect(res.data).not.toBeDefined()
+    expect(res.meta).not.toBeDefined()
+    expect(res.errors).toBeDefined()
+    expect(res.errors).toHaveLength(1)
+    expect(res.errors[0].message).toBe("ID must be a number")
+  })
+})
