328ac820277f833ecd8525932baac0c1a00ca143
Jan Henning <triton539@outlook.com>
2023-09-06 20:52:00 +0200

    Add tests for social profiles

---
+++ src/modules/v2/social/profiles/__tests__/getProfilePosts.test.ts
@@ -0,0 +1,185 @@
+import { server, getAuthCredentials } from "@/test-utils"
+import { db } from "@/utils"
+
+let TEST_USER_NAME = ""
+let BEARER_TOKEN = ""
+let API_KEY = ""
+
+beforeEach(async () => {
+  const { bearerToken, apiKey, name } = await getAuthCredentials()
+
+  BEARER_TOKEN = bearerToken
+  API_KEY = apiKey
+  TEST_USER_NAME = name
+
+  await db.socialPost.createMany({
+    data: [
+      {
+        title: "My post",
+        tags: ["tag1"],
+        owner: name
+      },
+      {
+        title: "Another post",
+        tags: ["another_tag"],
+        owner: name
+      }
+    ]
+  })
+})
+
+afterEach(async () => {
+  const media = db.media.deleteMany()
+  const apiKey = db.apiKey.deleteMany()
+  const users = db.userProfile.deleteMany()
+
+  await db.$transaction([media, apiKey, users])
+  await db.$disconnect()
+})
+
+describe("[GET] /v2/social/profiles/:id/posts", () => {
+  it("should return posts belonging to profile", async () => {
+    const response = await server.inject({
+      url: `/api/v2/social/profiles/${TEST_USER_NAME}/posts`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+        "X-Noroff-API-Key": API_KEY
+      }
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(200)
+    expect(res.data).toBeDefined()
+    expect(res.data[0].title).toBeDefined()
+    expect(res.data[1].title).toBeDefined()
+    expect(res.meta).toBeDefined()
+    expect(res.meta).toStrictEqual({
+      isFirstPage: true,
+      isLastPage: true,
+      currentPage: 1,
+      previousPage: null,
+      nextPage: null,
+      pageCount: 1,
+      totalCount: 2
+    })
+  })
+
+  it("should return posts with author profile, reactions, and comments", async () => {
+    const response = await server.inject({
+      url: `/api/v2/social/profiles/${TEST_USER_NAME}/posts?_author=true&_reactions=true&_comments=true`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+        "X-Noroff-API-Key": API_KEY
+      }
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(200)
+    expect(res.data).toBeDefined()
+    expect(res.data[0].title).toBeDefined()
+    expect(res.data[0].author).toBeDefined()
+    expect(res.data[0].reactions).toBeDefined()
+    expect(res.data[0].comments).toBeDefined()
+    expect(res.meta).toBeDefined()
+    expect(res.meta).toStrictEqual({
+      isFirstPage: true,
+      isLastPage: true,
+      currentPage: 1,
+      previousPage: null,
+      nextPage: null,
+      pageCount: 1,
+      totalCount: 2
+    })
+  })
+
+  it("should return posts that match a tag", async () => {
+    const response = await server.inject({
+      url: `/api/v2/social/profiles/${TEST_USER_NAME}/posts?_tag=tag1`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+        "X-Noroff-API-Key": API_KEY
+      }
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(200)
+    expect(res.data).toBeDefined()
+    expect(res.data).toHaveLength(1)
+    expect(res.data[0].title).toBeDefined()
+    expect(res.data[0].tags).toEqual(expect.arrayContaining(["tag1"]))
+    expect(res.meta).toBeDefined()
+    expect(res.meta).toStrictEqual({
+      isFirstPage: true,
+      isLastPage: true,
+      currentPage: 1,
+      previousPage: null,
+      nextPage: null,
+      pageCount: 1,
+      totalCount: 1
+    })
+  })
+
+  it("should throw 404 error when attempting to access profile that does not exist", async () => {
+    const response = await server.inject({
+      url: `/api/v2/social/profiles/does_not_exist/posts`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${BEARER_TOKEN}`,
+        "X-Noroff-API-Key": API_KEY
+      }
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(404)
+    expect(res.data).not.toBeDefined()
+    expect(res.meta).not.toBeDefined()
+    expect(res.errors).toBeDefined()
+    expect(res.errors).toHaveLength(1)
+    expect(res.errors[0]).toStrictEqual({
+      message: "No profile with this name"
+    })
+  })
+
+  it("should throw 401 error when attempting to access without API key", async () => {
+    const response = await server.inject({
+      url: `/api/v2/social/profiles/${TEST_USER_NAME}/posts`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${BEARER_TOKEN}`
+      }
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(401)
+    expect(res.data).not.toBeDefined()
+    expect(res.meta).not.toBeDefined()
+    expect(res.errors).toBeDefined()
+    expect(res.errors).toHaveLength(1)
+    expect(res.errors[0]).toStrictEqual({
+      message: "No API key header was found"
+    })
+  })
+
+  it("should throw 401 error when attempting to access without Bearer token", async () => {
+    const response = await server.inject({
+      url: `/api/v2/social/profiles/${TEST_USER_NAME}/posts`,
+      method: "GET",
+      headers: {
+        "X-Noroff-API-Key": API_KEY
+      }
+    })
+    const res = await response.json()
+
+    expect(response.statusCode).toBe(401)
+    expect(res.data).not.toBeDefined()
+    expect(res.meta).not.toBeDefined()
+    expect(res.errors).toBeDefined()
+    expect(res.errors).toHaveLength(1)
+    expect(res.errors[0]).toStrictEqual({
+      message: "No authorization header was found"
+    })
+  })
+})
