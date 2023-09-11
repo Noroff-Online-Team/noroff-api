434572896328da8d28288fdbc39beda139e86bf7
Jan Henning <triton539@outlook.com>
2023-09-11 12:42:35 +0200

    Remove and unregister @fastify/static as plugin

--- src/plugins/static.ts
+++
@@ -1,9 +0,0 @@
-import path from "path"
-import fp from "fastify-plugin"
-import fStatic from "@fastify/static"
-
-export default fp(async fastify => {
-  fastify.register(fStatic, {
-    root: path.join(__dirname, "../public")
-  })
-})
