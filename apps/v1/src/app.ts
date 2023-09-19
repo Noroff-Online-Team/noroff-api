f38ec556d6914a3d1d6560d787cb223b4862eada
Jan Henning <triton539@outlook.com>
2023-09-19 11:49:32 +0200

    v1: Throw err on startup, if any. Similar to v2

--- apps/v1/src/app.ts
+++ apps/v1/src/app.ts
@@ -7,9 +7,10 @@ const PORT = parseInt(process.env.PORT as string, 10) || 3000
 // Main startup
 async function main() {
   try {
-    server.listen({ port: PORT, host: "0.0.0.0" }, () =>
+    server.listen({ port: PORT, host: "0.0.0.0" }, err => {
+      if (err) throw err
       console.log(`> Noroff API v1 listening on http://localhost:${PORT}`)
-    )
+    })
   } catch (err) {
     console.error(err)
     process.exit(1)
