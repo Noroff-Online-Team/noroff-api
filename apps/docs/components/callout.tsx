7dc99014ed1596d8bd095d6a5149e419fc33b295
Jan Henning <triton539@outlook.com>
2023-09-20 17:40:49 +0200

    docs: Callout component updates

--- apps/docs/components/callout.tsx
+++ apps/docs/components/callout.tsx
@@ -1,28 +1,44 @@
-import { InfoIcon } from "lucide-react"
+import { InfoIcon, AlertCircle, AlertTriangle } from "lucide-react"
 import { Alert, AlertDescription } from "@/components/ui/alert"
 
-const GetEmoji = (variant: string): JSX.Element => {
+/**
+ * Returns an emoji based on the variant.
+ */
+const getEmoji = (variant: string): JSX.Element => {
   switch (variant) {
     case "info":
       return <InfoIcon className="h-4 w-4" />
+    case "warning":
+      return <AlertTriangle className="h-4 w-4" />
     case "destructive":
-      return <InfoIcon className="h-4 w-4" />
+      return <AlertCircle className="h-4 w-4" />
     default:
       return <InfoIcon className="h-4 w-4" />
   }
 }
 
+/**
+ * A small, stylized box that can be used to highlight a piece of information.
+ */
 export function Callout({
   children,
   variant = "info"
 }: {
   children: React.ReactNode
-  variant?: "default" | "info" | "destructive"
+  variant?: "default" | "info" | "warning" | "destructive"
 }) {
+  /**
+   * Determine if the children should be wrapped in a <p> tag.
+   * If the children is a string or an array (potentially of inline elements or text),
+   * a <p> tag is added for proper HTML structure. For JSX or complex React nodes,
+   * the children are rendered as-is without additional wrapping.
+   */
+  const shouldWrapInParagraph = typeof children === "string" || Array.isArray(children)
+
   return (
     <Alert variant={variant}>
-      {GetEmoji(variant)}
-      <AlertDescription>{children}</AlertDescription>
+      {getEmoji(variant)}
+      <AlertDescription>{shouldWrapInParagraph ? <p>{children}</p> : children}</AlertDescription>
     </Alert>
   )
 }
