2236b963c06c152d8996e3fc424043c864ca91ed
Jan Henning <triton539@outlook.com>
2023-09-20 11:47:58 +0200

    docs: Remove unused mdx plugins, remove transparent prop from Nav, fix sidebar banner version

--- apps/docs/components/nav.tsx
+++ apps/docs/components/nav.tsx
@@ -4,10 +4,9 @@ import { cn } from "@/utils/cn"
 import { cva } from "class-variance-authority"
 import { GithubIcon } from "lucide-react"
 import { Nav as OriginalNav } from "next-docs-ui/components"
-import { SidebarContext } from "next-docs-zeta/sidebar"
 import Link from "next/link"
 import { useParams } from "next/navigation"
-import { useContext, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 const item = cva("px-2 py-1 rounded-md transition-colors hover:text-accent-foreground", {
   variants: {
@@ -18,27 +17,9 @@ const item = cva("px-2 py-1 rounded-md transition-colors hover:text-accent-foreg
 })
 export function Nav() {
   const { version } = useParams()
-  const [isSidebarOpen] = useContext(SidebarContext)
-  const [transparent, setTransparent] = useState(true)
   const [swaggerUrl, setSwaggerUrl] = useState<string>(
     version === "v1" ? "https://api.noroff.dev/docs" : "https://v2.api.noroff.dev/docs"
   )
-
-  useEffect(() => {
-    if (isSidebarOpen) {
-      setTransparent(true)
-      return
-    }
-
-    const listener = () => {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      setTransparent(window.document.scrollingElement!.scrollTop < 30)
-    }
-
-    listener()
-    window.addEventListener("scroll", listener)
-    return () => window.removeEventListener("scroll", listener)
-  }, [isSidebarOpen])
 
   useEffect(() => {
     setSwaggerUrl(version === "v1" ? "https://api.noroff.dev/docs" : "https://v2.api.noroff.dev/docs")
@@ -63,7 +44,6 @@ export function Nav() {
           external: true
         }
       ]}
-      transparent={transparent}
     >
       <div className="bg-secondary/50 rounded-md border p-1 text-sm text-muted-foreground max-sm:absolute max-sm:left-[50%] max-sm:translate-x-[-50%]">
         <Link href="/docs/v1" className={cn(item({ active: version === "v1" }))}>
--- apps/docs/contentlayer.config.ts
+++ apps/docs/contentlayer.config.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { makeSource } from "contentlayer/source-files"
 import { createConfig } from "next-docs-zeta/contentlayer/configuration"
-import { remarkDynamicContent, structure } from "next-docs-zeta/mdx-plugins"
-import rehypeKatex from "rehype-katex"
+import { structure } from "next-docs-zeta/mdx-plugins"
 import remarkMath from "remark-math"
 
 const config = createConfig({
@@ -18,6 +16,4 @@ const config = createConfig({
 
 config.mdx!.remarkPlugins!.push(remarkMath)
 
-config.mdx!.remarkPlugins!.push(remarkDynamicContent)
-config.mdx!.rehypePlugins!.push(rehypeKatex)
 export default makeSource(config)
