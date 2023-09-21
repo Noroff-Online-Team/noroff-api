20d2ce7c048648ea6074299b8fc17e4e771f748e
Jan Henning <triton539@outlook.com>
2023-09-21 19:36:11 +0200

    docs: Update search component

--- apps/docs/components/search.tsx
+++ apps/docs/components/search.tsx
@@ -6,10 +6,10 @@ import { useParams } from "next/navigation"
 import { useState } from "react"
 
 export default function CustomSearchDialog(props: SearchDialogProps) {
-  const { version } = useParams()
-  const defaultTag = version === "v1" ? "v1" : "v2"
-  const [tag, setTag] = useState<string>()
-  const value = tag ?? defaultTag
+  const { version: versionFromParams } = useParams()
+  const defaultVersion = versionFromParams === "v1" ? "v1" : "v2"
+  const [version, setVersion] = useState<string>()
+  const value = version ?? defaultVersion
 
   return (
     <SearchDialog {...props} tag={value}>
@@ -17,22 +17,22 @@ export default function CustomSearchDialog(props: SearchDialogProps) {
         <button
           className={cn(
             "border px-2 py-0.5 rounded-md text-xs text-muted-foreground font-medium transition-colors",
-            value === "v1" && "text-secondary-foreground bg-secondary"
+            value === "v2" && "text-secondary-foreground bg-secondary"
           )}
-          onClick={() => setTag("v1")}
+          onClick={() => setVersion("v2")}
           tabIndex={-1}
         >
-          v1
+          v2
         </button>
         <button
           className={cn(
             "border px-2 py-0.5 rounded-md text-xs text-muted-foreground font-medium transition-colors",
-            value === "v2" && "text-secondary-foreground bg-secondary"
+            value === "v1" && "text-secondary-foreground bg-secondary"
           )}
-          onClick={() => setTag("v2")}
+          onClick={() => setVersion("v1")}
           tabIndex={-1}
         >
-          v2
+          v1
         </button>
       </div>
     </SearchDialog>
