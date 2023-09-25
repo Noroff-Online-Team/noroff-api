958c04477069de492059323d34b0c361c6376be4
Jan Henning <triton539@outlook.com>
2023-09-25 22:27:43 +0200

    docs: remove remark-math mdx plugin

--- apps/docs/contentlayer.config.ts
+++ apps/docs/contentlayer.config.ts
@@ -1,19 +1,4 @@
 import { makeSource } from "contentlayer/source-files"
-import { createConfig } from "next-docs-zeta/contentlayer/configuration"
-import { structure } from "next-docs-zeta/mdx-plugins"
-import remarkMath from "remark-math"
+import { defaultConfig } from "next-docs-zeta/contentlayer/configuration"
 
-const config = createConfig({
-  docsComputedFields: {
-    structuredData: {
-      type: "json",
-      resolve: page => {
-        return structure(page.body.raw, [remarkMath])
-      }
-    }
-  }
-})
-
-config.mdx!.remarkPlugins!.push(remarkMath)
-
-export default makeSource(config)
+export default makeSource(defaultConfig)
