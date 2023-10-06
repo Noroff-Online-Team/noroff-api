7c0d682b7397bba0bc7d4dcadb50e139ba849d83
Jan Henning <triton539@outlook.com>
2023-10-06 12:12:06 +0200

    add ts-eslint/recommended to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     jest: true
   },
-  extends: ["prettier", "eslint:recommended"],
+  extends: ["prettier", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
