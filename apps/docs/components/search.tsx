e8fce504f3085f5a67c2227e552dddeffdd255b7
Jan Henning <triton539@outlook.com>
2023-10-06 12:25:25 +0200

    docs: Update SearchDialog component for new version

--- apps/docs/components/search.tsx
+++ apps/docs/components/search.tsx
@@ -1,40 +1,46 @@
 "use client"
 
 import { cn } from "@/utils/cn"
-import SearchDialog, { type SearchDialogProps } from "next-docs-ui/components/dialog/search"
+import { cva } from "class-variance-authority"
+import { SearchDialog, type SharedProps } from "next-docs-ui/components/dialog/search"
+import { useDocsSearch } from "next-docs-zeta/search"
 import { useParams } from "next/navigation"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-export default function CustomSearchDialog(props: SearchDialogProps) {
-  const { version: versionFromParams } = useParams()
-  const defaultVersion = versionFromParams === "v1" ? "v1" : "v2"
-  const [version, setVersion] = useState<string>()
-  const value = version ?? defaultVersion
+const itemVariants = cva("border px-2 py-0.5 rounded-md text-xs text-muted-foreground font-medium transition-colors", {
+  variants: {
+    active: {
+      true: "text-accent-foreground bg-accent"
+    }
+  }
+})
+
+export default function CustomSearchDialog(props: SharedProps) {
+  const { version } = useParams()
+  const defaultTag = version === "v1" ? "v1" : "v2"
+  const [tag, setTag] = useState(defaultTag)
+  const { search, setSearch, query } = useDocsSearch("en-US", tag)
+
+  useEffect(() => {
+    setTag(defaultTag)
+  }, [defaultTag])
 
   return (
-    <SearchDialog {...props} tag={value}>
-      <div className="flex flex-row gap-1 px-4 py-2">
-        <button
-          className={cn(
-            "border px-2 py-0.5 rounded-md text-xs text-muted-foreground font-medium transition-colors",
-            value === "v2" && "text-secondary-foreground bg-secondary"
-          )}
-          onClick={() => setVersion("v2")}
-          tabIndex={-1}
-        >
-          v2
-        </button>
-        <button
-          className={cn(
-            "border px-2 py-0.5 rounded-md text-xs text-muted-foreground font-medium transition-colors",
-            value === "v1" && "text-secondary-foreground bg-secondary"
-          )}
-          onClick={() => setVersion("v1")}
-          tabIndex={-1}
-        >
-          v1
-        </button>
-      </div>
-    </SearchDialog>
+    <SearchDialog
+      {...props}
+      search={search}
+      onSearchChange={setSearch}
+      data={query.data}
+      footer={
+        <div className="flex flex-row gap-1 px-4 py-2">
+          <button className={cn(itemVariants({ active: tag === "v2" }))} onClick={() => setTag("v2")} tabIndex={-1}>
+            v2
+          </button>
+          <button className={cn(itemVariants({ active: tag === "v1" }))} onClick={() => setTag("v1")} tabIndex={-1}>
+            v1
+          </button>
+        </div>
+      }
+    />
   )
 }
