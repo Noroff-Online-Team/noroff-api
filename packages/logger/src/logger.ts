ecddb5b949af3c56437274335b4031bf820b4b5e
Jan Henning <triton539@outlook.com>
2023-10-26 12:42:09 +0200

    Update logger, remove logger .env.example

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,7 +1,4 @@
 import { pino } from "pino"
-// import pinoLoki from "pino-loki"
-
-const { LOKI_HOST_URL, LOKI_USERNAME, LOKI_PASSWORD, NODE_ENV } = process.env
 
 interface CreateLoggerOptions {
   label?: string
@@ -14,57 +11,24 @@ interface CreateLoggerOptions {
  * @returns A Pino logger instance with Loki transport
  */
 export function createLogger({ label }: CreateLoggerOptions = { label: "Noroff API" }) {
-  const isProduction = NODE_ENV === "production"
-
-  if (isProduction && (!LOKI_HOST_URL || !LOKI_USERNAME || !LOKI_PASSWORD)) {
-    throw new Error(
-      "Missing environment variables for Loki. Please check that LOKI_HOST_URL, LOKI_USERNAME and LOKI_PASSWORD are set."
-    )
-  }
-
-  switch (NODE_ENV) {
-    case "production":
-      return pino({
-        transport: {
-          targets: [
-            {
-              target: "pino-loki",
-              level: "info",
-              options: {
-                batching: true,
-                labels: { application: label },
-                host: process.env.LOKI_HOST_URL!,
-                basicAuth: {
-                  username: process.env.LOKI_USERNAME!,
-                  password: process.env.LOKI_PASSWORD!
-                }
-              }
-            },
-            {
-              target: "pino-pretty",
-              level: "info",
-              options: {}
-            }
-          ]
-        }
-      })
-    case "development":
-      return pino({
-        transport: {
+  return pino({
+    transport: {
+      targets: [
+        {
+          target: "pino-loki",
+          level: "info",
+          options: {
+            batching: true,
+            labels: { application: label },
+            host: "http://loki:3100"
+          }
+        },
+        {
           target: "pino-pretty",
           level: "info",
           options: {}
         }
-      })
-    case "test":
-      return pino({ enabled: false })
-    default:
-      return pino({
-        transport: {
-          target: "pino-pretty",
-          level: "info",
-          options: {}
-        }
-      })
-  }
+      ]
+    }
+  })
 }
