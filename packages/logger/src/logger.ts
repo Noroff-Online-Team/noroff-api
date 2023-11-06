d551f16031eeeb304bfe2e8775613db54d4e0d6d
Jan Henning <triton539@outlook.com>
2023-11-06 12:40:00 +0100

    Use env for loki, fixes logs not coming through

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,7 +1,5 @@
 import { pino } from "pino"
 
-const { NODE_ENV } = process.env
-
 interface CreateLoggerOptions {
   label?: string
 }
@@ -13,41 +11,21 @@ interface CreateLoggerOptions {
  * @returns A Pino logger instance with Loki transport
  */
 export function createLogger({ label }: CreateLoggerOptions = { label: "Noroff API" }) {
-  switch (NODE_ENV) {
-    case "test":
-      return pino({ level: "silent" })
-    case "development":
-      return pino({
-        transport: {
-          targets: [
-            {
-              target: "pino-pretty",
-              level: "debug",
-              options: {
-                colorize: true,
-                ignore: "pid,hostname"
-              }
-            }
-          ]
+  if (!process.env.LOKI_HOST) throw new Error("LOKI_HOST environment variable is required to create a logger")
+
+  return pino({
+    transport: {
+      targets: [
+        {
+          target: "pino-loki",
+          level: "info",
+          options: {
+            batching: true,
+            labels: { application: label },
+            host: process.env.LOKI_HOST
+          }
         }
-      })
-    case "production":
-      return pino({
-        transport: {
-          targets: [
-            {
-              target: "pino-loki",
-              level: "info",
-              options: {
-                batching: true,
-                labels: { application: label },
-                host: "http://noroff-loki:3100"
-              }
-            }
-          ]
-        }
-      })
-    default:
-      return pino({ level: "silent" })
-  }
+      ]
+    }
+  })
 }
