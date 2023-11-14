3851028189f168173127da656d01fd41adb00cd4
Jan Henning <triton539@outlook.com>
2023-11-14 23:57:12 +0100

    update source util for new versions, convert to createPageTreeBuilder from buildPageTree

--- apps/docs/utils/source.ts
+++ apps/docs/utils/source.ts
@@ -1,8 +1,9 @@
 import { allDocs, allMeta } from "contentlayer/generated"
 import { AxeIcon } from "lucide-react"
-import { buildPageTree, createUtils, loadContext } from "next-docs-zeta/contentlayer"
-import type { PageTree } from "next-docs-zeta/server"
+import { createUtils, loadContext } from "next-docs-zeta/contentlayer"
+import { type PageTree, createPageTreeBuilder } from "next-docs-zeta/server"
 import { createElement } from "react"
+import type { RawDocumentData } from "contentlayer/source-files"
 
 const ctx = loadContext(allMeta, allDocs, {
   resolveIcon() {
@@ -10,13 +11,25 @@ const ctx = loadContext(allMeta, allDocs, {
   }
 })
 
-const v1Tree = buildPageTree(ctx, {
-  root: "docs/v1"
+export const { getPage, getPageUrl } = createUtils(ctx)
+
+const builder = createPageTreeBuilder({
+  pages: allDocs.map(page => ({
+    file: getFileData(page._raw, page.locale),
+    title: page.title,
+    url: getPageUrl(page.slug.split("/"), page.locale),
+    icon: page.icon
+  })),
+  metas: allMeta.map(meta => ({
+    file: getFileData(meta._raw),
+    pages: meta.pages,
+    icon: meta.icon,
+    title: meta.title
+  }))
 })
 
-const v2Tree = buildPageTree(ctx, {
-  root: "docs/v2"
-})
+const v1Tree = builder.build({ root: "docs/v1" })
+const v2Tree = builder.build({ root: "docs/v2" })
 
 export function getTree(version: "v1" | "v2" | string): PageTree {
   if (version === "v1") {
@@ -26,4 +39,15 @@ export function getTree(version: "v1" | "v2" | string): PageTree {
   return v2Tree
 }
 
-export const { getPage, getPageUrl } = createUtils(ctx)
+function getFileData(raw: RawDocumentData, locale?: string) {
+  const dotIndex = raw.sourceFileName.lastIndexOf(".")
+  const flattenedPath = raw.sourceFileDir === raw.flattenedPath ? raw.flattenedPath + "/index" : raw.flattenedPath
+
+  return {
+    locale,
+    dirname: raw.sourceFileDir,
+    name: raw.sourceFileName.slice(0, dotIndex === -1 ? undefined : dotIndex),
+    flattenedPath,
+    path: raw.sourceFilePath
+  }
+}
