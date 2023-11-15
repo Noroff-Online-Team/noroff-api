843dc8320e3f1397d26156ec406fd1e92bd82edf
Jan Henning <triton539@outlook.com>
2023-11-15 11:11:05 +0100

    register all custom components in MDX

--- apps/docs/app/docs/[version]/[[...slug]]/content.tsx
+++ apps/docs/app/docs/[version]/[[...slug]]/content.tsx
@@ -1,20 +1,45 @@
 "use client"
 
-import { Hr } from "@/components/hr"
+import { EndpointDetails } from "@/components/endpoint-details"
+import { Callout } from "@/components/callout"
+import { TypeTable } from "next-docs-ui/components/type-table"
 import { useMDXComponent } from "next-contentlayer/hooks"
 import { ImageZoom } from "next-docs-ui/components/image-zoom"
 import defaultComponents from "next-docs-ui/mdx"
 import type { HTMLAttributes } from "react"
+
+type TypeTableObjectType = {
+  [name: string]: {
+    description?: string
+    type: string
+    typeDescription?: string
+    typeDescriptionLink?: string
+    default?: string
+  }
+}
+
+type CalloutType = {
+  variant?: "default" | "info" | "warning" | "destructive"
+  noIcon?: boolean
+}
+
+type EndpointDetailsType = {
+  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
+  path: string
+}
 
 const components = {
   ...defaultComponents,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   img: (props: any) => <ImageZoom {...props} />,
   pre: (props: HTMLAttributes<HTMLPreElement>) => <defaultComponents.pre {...props} className="max-h-[400px]" />,
-  Hr: (props: HTMLAttributes<HTMLHRElement>) => <Hr {...props} />,
   blockquote: (props: React.QuoteHTMLAttributes<HTMLQuoteElement>) => (
     <div className="my-4 rounded-lg border px-3 text-sm shadow-md">{props.children}</div>
-  )
+  ),
+  Hr: (props: HTMLAttributes<HTMLHRElement>) => <hr {...props} className="my-8 border-[hsl(var(--border))]" />,
+  EndpointDetails: (props: HTMLAttributes<HTMLDivElement> & EndpointDetailsType) => <EndpointDetails {...props} />,
+  TypeTable: (props: HTMLAttributes<HTMLDivElement> & { type: TypeTableObjectType }) => <TypeTable {...props} />,
+  Callout: (props: HTMLAttributes<HTMLDivElement> & CalloutType) => <Callout {...props}>{props.children}</Callout>
 }
 
 export function Content({ code }: { code: string }) {
