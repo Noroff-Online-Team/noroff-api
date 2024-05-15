9938e72f23c11cc4f163907747752f943a1fd8a3
Jan Henning <triton539@outlook.com>
2024-05-15 13:28:31 +0200

    assertion

--- apps/v2/src/plugins/rate-limit.ts
+++ apps/v2/src/plugins/rate-limit.ts
@@ -1,6 +1,5 @@
 import type { FastifyRequest } from "fastify"
 import rateLimit from "@fastify/rate-limit"
-import type { UserProfile } from "@prisma/v2-client"
 import fp from "fastify-plugin"
 
 export default fp(async fastify => {
@@ -8,7 +7,7 @@ export default fp(async fastify => {
     max: 600,
     timeWindow: "10 minutes",
     keyGenerator: (req: FastifyRequest) => {
-      const user = req.user as Pick<UserProfile, "name"> | null
+      const user = req.user as { name: string } | null
       const trueClientIp = (req.headers["true-client-ip"] as string) || req.ip
       const userAgent = req.headers["user-agent"] as string
 
