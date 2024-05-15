39888809aeffd45d4f71adc5be1059d01b6b5ddc
Jan Henning <triton539@outlook.com>
2024-05-15 13:21:40 +0200

    fix for v2

--- apps/v2/src/plugins/rate-limit.ts
+++ apps/v2/src/plugins/rate-limit.ts
@@ -1,11 +1,18 @@
 import type { FastifyRequest } from "fastify"
 import rateLimit from "@fastify/rate-limit"
+import type { UserProfile } from "@prisma/v2-client"
 import fp from "fastify-plugin"
 
 export default fp(async fastify => {
   fastify.register(rateLimit, {
     max: 600,
     timeWindow: "10 minutes",
-    keyGenerator: (req: FastifyRequest) => (req.headers["true-client-ip"] as string) || req.ip
+    keyGenerator: (req: FastifyRequest) => {
+      const user = req.user as Pick<UserProfile, "name"> | null
+      const trueClientIp = (req.headers["true-client-ip"] as string) || req.ip
+      const userAgent = req.headers["user-agent"] as string
+
+      return user ? `${trueClientIp}-${userAgent}-${user.name}` : `${trueClientIp}-${userAgent}`
+    }
   })
 })
