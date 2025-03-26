0ccfaaf71c4dcf108bdbfb84757fa12d529144b4
Jan Henning <triton539@outlook.com>
2025-03-26 15:29:25 +0100

    Adds OpenAPI config for Swagger docs

---
+++ apps/v3/src/lib/configure-open-api.ts
@@ -0,0 +1,30 @@
+import { apiReference } from "@scalar/hono-api-reference"
+
+import type { AppOpenAPI } from "./types"
+
+import packageJSON from "../../package.json" with { type: "json" }
+
+export default function configureOpenAPI(app: AppOpenAPI) {
+  app.doc("/openapi.json", {
+    openapi: "3.0.0",
+    info: {
+      version: packageJSON.version,
+      title: "Noroff API"
+    }
+  })
+
+  app.get(
+    "/docs",
+    apiReference({
+      theme: "kepler",
+      layout: "classic",
+      defaultHttpClient: {
+        targetKey: "js",
+        clientKey: "fetch"
+      },
+      spec: {
+        url: "/openapi.json"
+      }
+    })
+  )
+}
